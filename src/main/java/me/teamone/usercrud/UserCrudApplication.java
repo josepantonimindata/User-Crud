0a256f7372f26fff32afb7caa8777748cddb198b
Josep Aguilo <josep.aguilo@mindata.es>
2024-04-16 16:32:51 +0200

    Initial Commit

---
+++ src/main/java/me/teamone/usercrud/UserCrudApplication.java
@@ -0,0 +1,13 @@
+package me.teamone.usercrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/teamone/usercrud/UserCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package me.teamone.usercrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
