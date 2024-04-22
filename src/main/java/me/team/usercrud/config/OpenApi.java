9f7ec6e440cf12804c7daff0caca20580c2ee979
Josep Aguilo <josep.aguilo@mindata.es>
2024-04-22 12:19:28 +0200

    Add OpenAPI definition and Swagger annotations
    Added an OpenAPI definition for the User Proxy API Database and incorporated Swagger annotations into the UserCreateController.
    
    This update allows Swagger UI to document and visualize the API's resources. The OpenAPI definition file describes the structure of the API, while Swagger annotations detail endpoint-specific information. Maven dependencies and plugins related with OpenAPI and Swagger were also added to pom.xml.

---
+++ src/main/java/me/team/usercrud/config/OpenApi.java
@@ -0,0 +1,13 @@
+package me.team.usercrud.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+    info = @Info(
+        title = "User Proxy API Database",
+        version = "1.0.0",
+        description = "Proxy API - Service of the database CRUD & Criteria - User"
+    )
+)
+public class OpenApi {}
