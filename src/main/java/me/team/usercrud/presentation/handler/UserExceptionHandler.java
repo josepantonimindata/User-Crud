96662bfae4c14eccf052ab45dcfd40e4488a631b
Josep Aguilo <josep.aguilo@mindata.es>
2024-04-23 11:34:48 +0200

    Improve Exception handling Added RFC 9457
    Added a new endpoint to enable user search functionality in the application. Improved exception handling using `ProblemDetails` for more detailed error responses. `UserExceptionHandler.java` was moved to presentation layer to enhance logical structure and separation of concern.

---
+++ src/main/java/me/team/usercrud/presentation/handler/UserExceptionHandler.java
@@ -0,0 +1,33 @@
+package me.team.usercrud.presentation.handler;
+
+import me.team.usercrud.application.user.exceptions.UserAlreadyExistsException;
+import me.team.usercrud.application.user.exceptions.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class UserExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ProblemDetail> handleGenericException(Exception ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+        return new ResponseEntity<>(problemDetail, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ProblemDetail> handleUserNotFoundException(UserNotFoundException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        return new ResponseEntity<>(problemDetail, HttpStatus.NOT_FOUND);
+    }
+
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<ProblemDetail> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.getMessage());
+        return new ResponseEntity<>(problemDetail, HttpStatus.CONFLICT);
+    }
+}
